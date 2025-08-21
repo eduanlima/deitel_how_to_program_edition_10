36506713a475e4a4032f4854af1da67ac357b008
eduanlima@github.com <eduandev@gmail.com>
2025-08-21 19:02:30 -0300

    Create class SalariedEmployee

---
+++ src/chapter_10_polymorphism_and_interfaces/SalariedEmployee.java
@@ -0,0 +1,40 @@
+package chapter_10_polymorphism_and_interfaces;
+
+public class SalariedEmployee extends Employee {
+    private double weeklySalary;
+
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber) {
+        super(firstName, lastName, socialSecurityNumber);
+
+        if (weeklySalary < 0.0)
+            throw new IllegalArgumentException(
+                "Weekly salary must be >= 0.0"
+            );
+
+        this.weeklySalary = weeklySalary;
+    }
+
+    //Configure salary
+    public void setWeeklySalary(double weeklySalary) {
+        if (weeklySalary < 0.0)
+            throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+
+        this.weeklySalary = weeklySalary;
+    }
+
+    public double getWeeklySalary() {
+        return weeklySalary;
+    }
+
+    //Overrides the method earnings from class Employee
+    @Override
+    public double earnings() {
+        return weeklySalary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Salaried employee: %s%n%s: $%,.2f",
+            super.toString(), "weekly salary", getWeeklySalary());
+    }
+}
