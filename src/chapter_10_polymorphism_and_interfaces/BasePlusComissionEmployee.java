ba9970b76417c68c084c8ce64c61cc9461198bc4
eduanlima@github.com <eduandev@gmail.com>
2025-08-25 22:21:44 -0300

    Created class BasePlusComissionEmployee

---
+++ src/chapter_10_polymorphism_and_interfaces/BasePlusComissionEmployee.java
@@ -0,0 +1,36 @@
+package chapter_10_polymorphism_and_interfaces;
+
+public class BasePlusComissionEmployee extends ComissionEmployee {
+    private double baseSalary;
+
+    public BasePlusComissionEmployee(String firstName, String lastName, String socialSecurityNumber, double baseSalary) {
+        super(firstName, lastName, socialSecurityNumber);
+        
+        if (baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+        
+        this.baseSalary = baseSalary;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        if (baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be >= 0.0");
+
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+    
+    @Override
+    public double earnings() {
+        return getBaseSalary() + super.earnings();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; %s: $%.2f", "base-salaried", super.toString(),
+        "base salary", getBaseSalary());
+    }    
+}
